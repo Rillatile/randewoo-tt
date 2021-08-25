3eaab45986288fddb44050488ca1e843428a620b
Roman Malinkov <malinkovrf@kadastr.ru>
2021-08-25 16:57:37 +0700

    Реализовал сохранение и получение значений настроек

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,14 @@
 class MainController < ApplicationController
   def index
+    if request.post?
+      Setting.update(params.slice(:schedule, :min_time, :max_time, :quantity))
+    end
+
+    @settings = {
+      schedule: Setting.get_value(:schedule),
+      min_time: Setting.get_value(:min_time),
+      max_time: Setting.get_value(:max_time),
+      quantity: Setting.get_value(:quantity) || 0
+    }
   end
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,9 @@
 class Setting < ApplicationRecord
+  def self.get_value(name)
+    Setting.find_by(name: name)&.value
+  end
+
+  def self.update(params)
+    params.keys.each { |key| Setting.find_or_create_by(name: key).update_attribute(:value, params[key]) }
+  end
 end
