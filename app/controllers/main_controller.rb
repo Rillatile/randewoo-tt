c3aabae4b2f25fbd92e98b124aaefdcfe7604aad
Roman Malinkov <roman.malinkov@list.ru>
2021-08-27 21:19:13 +0700

    Добавил проверку корректности cron

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,10 +4,16 @@ require 'settings'
 class MainController < ApplicationController
   def index
     if request.post?
-      Settings.update(params.slice(:schedule, :min_time, :max_time, :quantity))
-      Scheduler.update
+      if Scheduler.correct_cron?(params[:schedule])
+        Settings.update(params.slice(:schedule, :min_time, :max_time, :quantity))
+        Scheduler.update
+        @error = nil
+      else
+        @error = 'Расписание не соответствует формату CRON!'
+      end
     end
 
     @settings = Settings.current_settings
+    @settings[:schedule] = params[:schedule] if @error.present?
   end
 end
--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -1,4 +1,10 @@
 module Scheduler
+  def self.correct_cron?(cron)
+    cron_regex = /(?i-mx:^(@(reboot|yearly|annually|monthly|weekly|daily|midnight|hourly)|((\*?[\d\/,\-]*)\s){3}(\*?([\d\/,\-]|(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec))*\s)(\*?([\d\/,\-]|(sun|mon|tue|wed|thu|fri|sat))*))$)/
+
+    !!cron.match(cron_regex)
+  end
+
   def self.update
     system('whenever --update-crontab')
     Rails.logger.info("#{Time.now}, INFO, \"The schedule has been updated with a new value '#{Redis.current.get('schedule')}'\"")
