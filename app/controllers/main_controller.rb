d578849ff70e7e87f8bd38d76f4b377289245c32
Roman Malinkov <roman.malinkov@list.ru>
2021-08-28 20:57:31 +0700

    Добавил обработку исключений при взаимодействии с Redis

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,14 +5,31 @@ class MainController < ApplicationController
   def index
     if request.post?
       if Scheduler.correct_cron?(params[:schedule])
-        Settings.update(params.slice(:schedule, :min_time, :max_time, :quantity, :messages_receiver_url))
-        Scheduler.update
+        if Settings.update(params.slice(:schedule, :min_time, :max_time, :quantity, :messages_receiver_url))
+          Scheduler.update
+        else
+          @error = 'Произошла ошибка при взаимодействии с Redis!'
+          status = :internal_server_error
+        end
       else
         @error = 'Расписание не соответствует формату CRON!'
+        status = :bad_request
       end
     end
 
-    @settings = Settings.current_settings
-    @settings[:schedule] = params[:schedule] if @error.present?
+    begin
+      @settings = Settings.current_settings
+    rescue Redis::CannotConnectError
+      @settings = Settings.empty_settings
+      @error = 'Произошла ошибка при взаимодействии с Redis!'
+      status = :internal_server_error
+    rescue => error
+      @settings = Settings.empty_settings
+      @error = error.message
+      status = :internal_server_error
+    end
+
+    @settings = params.slice(:schedule, :min_time, :max_time, :quantity, :messages_receiver_url) if @error.present?
+    render status: status || :ok
   end
 end
--- lib/settings.rb
+++ lib/settings.rb
@@ -1,16 +1,44 @@
 module Settings
   def self.update(params)
-    params.keys.each { |key| Redis.current.set(key, params[key]) }
-    Rails.logger.info("#{Time.now}, INFO, \"The settings have been updated: #{params} \"")
+    begin
+      params.keys.each { |key| Redis.current.set(key, params[key]) }
+    rescue Redis::CannotConnectError
+      Rails.logger.error("#{Time.now}, ERROR, \"Connection to Redis could not be established\"")
+      return false
+    rescue => error
+      Rails.logger.error("#{Time.now}, ERROR, \"#{error.message}\"")
+      return false
+    end
+    Rails.logger.info("#{Time.now}, INFO, \"The settings have been updated: #{params}\"")
+    true
   end
 
   def self.current_settings
+    begin
+      current_settings = {
+        schedule: Redis.current.get('schedule'),
+        min_time: Redis.current.get('min_time'),
+        max_time: Redis.current.get('max_time'),
+        quantity: Redis.current.get('quantity'),
+        messages_receiver_url: Redis.current.get('messages_receiver_url')
+      }
+    rescue Redis::CannotConnectError
+      Rails.logger.error("#{Time.now}, ERROR, \"Connection to Redis could not be established\"")
+      raise Redis::CannotConnectError
+    rescue => error
+      Rails.logger.error("#{Time.now}, ERROR, \"#{error.message}\"")
+      raise error
+    end
+    current_settings
+  end
+
+  def self.empty_settings
     {
-      schedule: Redis.current.get('schedule'),
-      min_time: Redis.current.get('min_time'),
-      max_time: Redis.current.get('max_time'),
-      quantity: Redis.current.get('quantity'),
-      messages_receiver_url: Redis.current.get('messages_receiver_url')
+      schedule: nil,
+      min_time: nil,
+      max_time: nil,
+      quantity: nil,
+      messages_receiver_url: nil
     }
   end
 end
