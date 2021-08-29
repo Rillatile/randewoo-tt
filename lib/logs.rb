2ffffdcb56b3a18226140309deec0b3bd2cfc0aa
Roman Malinkov <roman.malinkov@list.ru>
2021-08-29 20:53:33 +0700

    Фикс логирования

--- lib/logs.rb
+++ lib/logs.rb
@@ -1,7 +1,7 @@
 module Logs
   def self.get_logs
     logs = File.readlines("log/#{ENV['RAILS_ENV']}.log")
-    logs.delete_if { |line| !line.ends_with?(' - Application log') }
+    logs.delete_if { |line| !line.include?(' - Application log') }
     logs.map { |line| line.gsub(' - Application log', '') }
   end
 end
