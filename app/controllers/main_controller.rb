ad449b4490ce42e16e6c724c809ea2db5a18106a
Roman Malinkov <roman.malinkov@list.ru>
2021-08-29 17:50:22 +0700

    Оптимизация работы контроллера 'MainController'

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,4 @@
+require 'logs'
 require 'scheduler'
 require 'settings'
 
@@ -7,6 +8,7 @@ class MainController < ApplicationController
       if Scheduler.correct_cron?(params[:schedule])
         if Settings.update(params.slice(:schedule, :min_time, :max_time, :quantity, :messages_receiver_url))
           Scheduler.update
+          @success = 'Успешно!'
         else
           @error = 'Произошла ошибка при взаимодействии с Redis!'
           status = :internal_server_error
@@ -15,21 +17,26 @@ class MainController < ApplicationController
         @error = 'Расписание не соответствует формату CRON!'
         status = :bad_request
       end
+
+      @settings = params.slice(:schedule, :min_time, :max_time, :quantity, :messages_receiver_url)
+    else
+      begin
+        @settings = Settings.current_settings
+      rescue Redis::CannotConnectError
+        @settings = Settings.empty_settings
+        @error = 'Произошла ошибка при взаимодействии с Redis!'
+        status = :internal_server_error
+      rescue => error
+        @settings = Settings.empty_settings
+        @error = error.message
+        status = :internal_server_error
+      end
     end
 
-    begin
-      @settings = Settings.current_settings
-    rescue Redis::CannotConnectError
-      @settings = Settings.empty_settings
-      @error = 'Произошла ошибка при взаимодействии с Redis!'
-      status = :internal_server_error
-    rescue => error
-      @settings = Settings.empty_settings
-      @error = error.message
-      status = :internal_server_error
-    end
-
-    @settings = params.slice(:schedule, :min_time, :max_time, :quantity, :messages_receiver_url) if @error.present?
     render status: status || :ok
+  end
+
+  def logs
+    render json: { data: Logs.get_logs }
   end
 end
